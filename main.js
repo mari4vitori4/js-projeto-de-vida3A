f96ae47c9a4ef09938902363957f05f436151597
mari4vitori4 <159458866+mari4vitori4@users.noreply.github.com>
2026-04-10 07:48:02 -0300

    tempoAtual

--- main.js
+++ main.js
@@ -13,7 +13,10 @@ for(let i = 0; i < botoes.length; i = i + 1){
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
-
 }
+const tempoObjetivo01 = new Date("2026-10=05T00:00:00");
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo01 - tempoAtual;
+
 console.log(botoes);
          
